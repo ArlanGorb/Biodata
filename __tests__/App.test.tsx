fa6766ed6fc98efe6eef748381bb1f2e9708ebef
ArlanGorb <s22310278@student.unklab.ac.id>
2025-04-08 21:21:43 +0800

    exercise 7

--- __tests__/App.test.tsx
+++ __tests__/App.test.tsx
@@ -4,7 +4,7 @@
 
 import React from 'react';
 import ReactTestRenderer from 'react-test-renderer';
-import App from '../App';
+import App from '../Exercise_7';
 
 test('renders correctly', async () => {
   await ReactTestRenderer.act(() => {
